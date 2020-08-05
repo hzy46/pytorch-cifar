1a7e1c9884b2df5cd2d0037d555a7c09c95b633b
Zhiyuan He <zhiyuhe@microsoft.com>
2020-08-05 14:18:08 +0800

    fix

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from utils import progress_bar
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--batch_size', default=128, type=int, help='total batch size')
+parser.add_argument('--val_batch_size', default=128, type=int, help='val batch size')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 parser.add_argument('--mode', choices=['SG', 'MGSN_DP', 'MGSN_DDP', 'MGMN_DDP'], 
@@ -52,12 +54,12 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=128, shuffle=False, num_workers=2)
+    testset, batch_size=args.val_batch_size, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
