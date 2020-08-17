e3114a43bd59983528288021c8e9d1d31ab0a087
Zhiyuan He <zhiyuhe@microsoft.com>
2020-08-17 15:43:32 +0800

    fix

--- MN_DDP.py
+++ MN_DDP.py
@@ -128,7 +128,7 @@ def main(local_world_size, local_rank):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr,
                           momentum=0.9, weight_decay=5e-4)
-
+    print('==> Start Epoch...')
     for epoch in range(start_epoch, start_epoch + 200):
         # train
         print('\n[%d]Epoch: %d' % (rank, epoch))
